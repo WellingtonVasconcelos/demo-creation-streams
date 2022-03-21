50c92860ef2c5e43fcbed26ee99151100a2c1bf7
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-03-21 10:00:00 -0300

    Third stream

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,17 @@ public class Program {
 		
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 		
+		//Criando uma STREAM apartir de uma coleção (LIST)
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
 		
+		// Criando uma STREAM usando o Stream.of
 		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
 		System.out.println(Arrays.toString(st2.toArray()));
+		
+		// Criando uma STREAM usando a função de iteração
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));
 	}
 
 }
